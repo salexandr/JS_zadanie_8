9e3f632ec7e264ad96eba5d4cdd0b73cbf7b18b5
Alexandr <a.senia@yandex.ru>
2016-08-22 14:05:03 +0400

    gotovoe

---
+++ js/script.js
@@ -0,0 +1,20 @@
+﻿  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+  
+    $( function() {
+    var tooltips = $( "[title]" ).tooltip({
+      position: {
+        my: "left top",
+        at: "right+5 top-5",
+        collision: "none"
+      }
+    });
+    $( "<button>" )
+      .text( "Show help" )
+      .button()
+      .on( "click", function() {
+        tooltips.tooltip( "open" );
+      })
+      .insertAfter( "form" );
+  } );
